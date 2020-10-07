8cba26952c164ea87203ab5a6f087e769ef0893a
ArtZhiv <zhiv-art@yandex.com>
2020-10-07 17:43:33 +0300

    master

---
+++ inputsql/CONV.go
@@ -0,0 +1,80 @@
+package inputsql
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// enb2 ...
+type enb2 struct {
+	id         int
+	number     int
+	address    string
+	vendor     string
+	region     string
+	province   string
+	demolition string
+	place      string
+}
+
+// Convert ...
+func Convert(val string) {
+	a, _ := strconv.Atoi(val)
+	b := a % 100
+	bb := strconv.Itoa(b)
+
+	i, _ := strconv.ParseInt(bb, 16, 64)
+	m := strconv.FormatInt(i, 10)
+
+	c := (a / 100) % 100000
+	cc := strconv.Itoa(c)
+
+	ii, _ := strconv.ParseInt(cc, 16, 64)
+	mm := strconv.FormatInt(ii, 10)
+
+	db, err := sql.Open("mysql", "Artem:Artem$mena@tcp(192.168.37.64:3306)/beCloud_database")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("select * from beCloud_database.eNodeB where number like ?", mm)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	products := []enb2{}
+
+	for rows.Next() {
+		p := enb2{}
+		err := rows.Scan(&p.id, &p.number, &p.address, &p.vendor, &p.region, &p.province, &p.demolition, &p.place)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		products = append(products, p)
+	}
+	if len(products) == 0 {
+		dt := time.Now()
+		d := dt.Format("01.02.2006")
+		fmt.Println(mm, "не в коммерции на", d)
+	} else {
+		for _, p := range products {
+			if p.demolition != "___" {
+				len := len(p.demolition) - 4
+				fmt.Println("+ -------------- + ---------- +", strings.Repeat("-", len), "+")
+				fmt.Println("| ДЕМОНТИРОВАНА--|--", p.number, "--|--", p.demolition, "--|")
+				fmt.Println("+ -------------- + ---------- +", strings.Repeat("-", len), "+")
+				fmt.Println()
+			} else {
+				fmt.Println("Коллеги, данный клиент подключен к", m, "сектору на eNodeB", p.number, p.address)
+				fmt.Println()
+			}
+		}
+	}
+}
+
+// 25706 93864 17
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
 	"./inputsql"
 
@@ -9,9 +11,12 @@ import (
 )
 
 func main() {
+	cls()
+
 	var vvod int
-	fmt.Print("Введите цифру требуемого действия:\n\t1-обновление eNB;\n\t2-обновление секторов;\n\t3-вывод информации о eNB;\n_:  ")
+	fmt.Print("Введите цифру требуемого действия:\n\t1-обновление eNB;\n\t2-обновление секторов;\n\t3-вывод информации о eNB;\n\t4-Перевод для UNET (из HEX в DEC)\n_:  ")
 	fmt.Scan(&vvod)
+	fmt.Println()
 	switch {
 	case vvod == 1:
 		inputsql.InputENB()
@@ -20,5 +25,17 @@ func main() {
 	case vvod == 3:
 		fmt.Print("Введите номер eNodeB: ")
 		inputsql.Search()
+	case vvod == 4:
+		var inp string
+		fmt.Print("Введите значение из запроса: ")
+		fmt.Scan(&inp)
+		fmt.Println()
+		inputsql.Convert(inp)
 	}
+}
+
+func cls() {
+	cmd := exec.Command("powershell", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
