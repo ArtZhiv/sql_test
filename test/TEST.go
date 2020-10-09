ac97b0a4eae07f4e9b534642de0fc52e4b19ba05
ArtZhiv <zhiv-art@yandex.com>
2020-10-09 05:54:11 +0300

    master

--- test/TEST.go
+++ test/TEST.go
@@ -1,14 +1,49 @@
 package test
 
 import (
+	"fmt"
+	"io"
 	"os"
-	"os/exec"
 )
 
 // Test ...
 func Test() {
 	// var nummmm []string
+	// nummmm := make([]string, 1024)
 	// var a string
+
+	// fmt.Println("Сейчас откроется текстовый файл вставьте то что прислал МТС и сохраните файл")
+	// cmd := exec.Command("powershell", "/c", "./test.txt")
+	// cmd.Stdout = os.Stdout
+	// cmd.Run()
+
+	file, err := os.Open("test.txt")
+	if err != nil {
+		panic(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	data := make([]byte, 1024)
+	var n int
+	// var nummmm []string
+
+	for {
+		n, err = file.Read(data)
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(n)
+		fmt.Println(string(data[6:10]))
+		vvv := string(data[:n])
+		fmt.Println()
+		fmt.Println(vvv)
+
+		for _, elem := range vvv {
+			fmt.Println("ELEM:", elem)
+		}
+	}
+
 	// for {
 	// 	fmt.Scan(&a)
 	// 	if a == "*" {
@@ -21,13 +56,19 @@ func Test() {
 	// for idx, elem := range nummmm {
 	// 	fmt.Println("IDX:", idx, "ELEM:", elem)
 	// }
+
 	// if strings.Contains(a, "*") {
 	// 	fmt.Println("Massive")
 	// } else {
 	// 	fmt.Println("String", a)
 	// 	break
 	// }
-	cmd := exec.Command("powershell", "/c", "./test.txt")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
+
+	// var a string
+	// fmt.Scanln(&a)
+	// if a == "" {
+	// 	fmt.Println("Enter")
+	// } else {
+	// 	fmt.Println("Error")
+	// }
 }
